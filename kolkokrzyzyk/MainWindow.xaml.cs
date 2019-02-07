0e4641dc881349af7cbbf8484a43a94a041cc3e0
davidwzz <dawid.jan.wozniak@gmail.com>
2019-02-07 15:28:06 +0100

    Basic 9x9 grid and new game clean-up created.

---
+++ kolkokrzyzyk/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace kolkokrzyzyk
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Private Members
+
+        /// <summary>
+        /// Holds the current resultf of cells in active game
+        /// </summary>
+        private MarkType[] mResults;
+        
+        /// <summary>
+        /// True if its player 1's turn (X) or player 2's turn (O)
+        /// </summary>
+        private bool mPlayer1Turn;
+
+        /// <summary>
+        ///  true if the game has ended
+        /// </summary>
+        private bool mGameEnded;
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            NewGame();
+
+        }
+
+
+
+        #endregion
+
+        private void NewGame()
+        {
+            // Create a new blank array of free cells
+            mResults = new MarkType[81];
+
+            for (var i = 0; i < mResults.Length; i++)
+                mResults[i] = MarkType.Free;
+
+            // Make sure p[layer 1 is current player
+            mPlayer1Turn = true;
+
+            // access every button in the grid
+            Container.Children.Cast<Button>().ToList().ForEach(button =>
+            {
+                // change background, foreground and content to default values
+                button.Content = string.Empty;
+                button.Background = Brushes.White;
+                button.Foreground = Brushes.Blue;
+
+            });
+
+            //make sure the game hasn't finished
+            mGameEnded = false;
+
+        }
+    }
+}
---
+++ kolkokrzyzyk/MarkType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kolkokrzyzyk
+{   
+    /// <summary>
+    /// The type of value the cell in game have
+    /// </summary>
+    public enum MarkType
+    {
+        /// <summary>
+        /// cell wasnt clicked
+        /// </summary>
+        Free,
+        /// <summary>
+        /// cell is a O
+        /// </summary>
+        Nought,
+        /// <summary>
+        /// cell is an X
+        /// </summary>
+        Cross
+    }
+}
